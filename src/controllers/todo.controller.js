2199bc4cb3a1a67f79aa75d4424dc359f990a85a
ockode <ockode@tuta.io>
2023-08-03 16:52:43 +0200

    step 2

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,9 +1,45 @@
-const getAll = (req, res) => {
-  res.json({ todos: ["get all todos", "do this"] });
+import fs from "node:fs/promises";
+import todos from "../data/todo.data.js";
+
+const { readFile, writeFile } = fs;
+
+const CURRENT_DIR = process.cwd();
+
+const getAll = async (req, res) => {
+  let jsonData = null;
+
+  try {
+    jsonData = await readFile(`${CURRENT_DIR}/src/data/todo.data.json`, "utf8");
+  } catch (e) {
+    console.error(e.message);
+  }
+
+  const data = JSON.parse(jsonData);
+
+  res.json({ todos: data.todos });
+};
+
+const getAll_ = (req, res) => {
+  const data = todos;
+  res.json({ todos: data });
 };
 
 const create = (req, res) => {
-  res.status(401).json({ createdTodo: { id: 3243726478324, value: "my Todo" } });
+  // const id = req.body.todoId;
+  // const value = req.body.value;
+  // const completed = req.body.completed;
+
+  const { todoId, value, completed } = req.body;
+
+  const todo = {
+    id: todoId,
+    value,
+    completed,
+  };
+
+  todos.push(todo);
+
+  res.status(201).json({ message: `Todo successfully created`, todoCreated: todo });
 };
 
 export const TodoController = {
---
+++ src/data/todo.data.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 9238490324,
+    value: "9i3921 0i1as djhasdas",
+    completed: false,
+  },
+  {
+    id: 274032958,
+    value: "iasjd sa dahj",
+    completed: false,
+  },
+  {
+    id: 92967865,
+    value: "cbvx voix vxoc hvhj",
+    completed: false,
+  },
+];
+
+export default todos;
